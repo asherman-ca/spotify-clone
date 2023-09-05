8930f227bc99f0db572e9b829025e4b311187845
Alex Sherman <asherman.ca@gmail.com>
2023-09-04 21:17:11 -0700

    supabase types

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import Header from '@/components/Header'
 import ListItem from '@/components/ListItem'
 
@@ -34,6 +36,12 @@ export default function Home() {
 					</div>
 				</div>
 			</Header>
+			<div className='mt-2 mb-7 px-6'>
+				<div className='flex justify-between items-center'>
+					<h1 className='text-white text-2xl font-semibold'>Newest songs</h1>
+				</div>
+				{/* <PageContent songs={songs} /> */}
+			</div>
 		</div>
 	)
 }
