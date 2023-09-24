8f390c1fe3dd2c53080deee6d91e39ca97fbf57c
Alex Sherman <asherman.ca@gmail.com>
2023-09-24 12:19:01 -0700

    feature - error and loading components for SSR

--- actions/getSongs.ts
+++ actions/getSongs.ts
@@ -3,7 +3,15 @@ import { cookies } from 'next/headers'
 
 import { Song } from '@/types'
 
+// const dummyProm = new Promise((resolve, reject) => {
+// 	setTimeout(() => {
+// 		resolve(true)
+// 	}, 10000)
+// })
+
 const getSongs = async (): Promise<Song[]> => {
+	// await dummyProm
+
 	const supabase = createServerComponentClient({
 		cookies: cookies,
 	})
