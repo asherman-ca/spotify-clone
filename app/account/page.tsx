e67b1a4eb16cc82ab45be24753b4258381485f0e
Alex Sherman <asherman.ca@gmail.com>
2023-09-24 14:35:42 -0700

    feature - account page with customer portal link and sub modal

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -1,16 +1,46 @@
-import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
-import React from 'react'
-import { cookies } from 'next/headers'
+// import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
+// import React from 'react'
+// import { cookies } from 'next/headers'
 
-const page = async () => {
-	const supabase = createServerComponentClient({
-		cookies: cookies,
-	})
+// const page = async () => {
+// 	const supabase = createServerComponentClient({
+// 		cookies: cookies,
+// 	})
 
-	const { data: sessionData, error: sessionError } =
-		await supabase.auth.getSession()
+// 	const { data: sessionData, error: sessionError } =
+// 		await supabase.auth.getSession()
 
-	return <div>page</div>
+// 	return <div>page</div>
+// }
+
+// export default page
+
+import Header from '@/components/Header'
+
+import AccountContent from './components/AccountContent'
+
+const Account = () => {
+	return (
+		<div
+			className='
+        bg-neutral-900 
+        rounded-lg 
+        h-full 
+        w-full 
+        overflow-hidden 
+        overflow-y-auto
+      '
+		>
+			<Header className='from-bg-neutral-900'>
+				<div className='mb-2 flex flex-col gap-y-6'>
+					<h1 className='text-white text-3xl font-semibold'>
+						Account Settings
+					</h1>
+				</div>
+			</Header>
+			<AccountContent />
+		</div>
+	)
 }
 
-export default page
+export default Account
